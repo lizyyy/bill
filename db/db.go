a8eb3645dbb90b227decbcb347c3cafb29351179
lzy <lizhiyuan01@zuoyebang.com>
2026-03-13 19:43:57 +0800

    fix: update bill initialization and form handling

--- db/db.go
+++ db/db.go
@@ -34,40 +34,37 @@ func initDefaultCategories() {
 		return
 	}
 
-	defaultCategories := []models.Category{
-		{Name: "饮食"},
-		{Name: "交通"},
-		{Name: "购物"},
-		{Name: "娱乐"},
-		{Name: "医疗"},
-		{Name: "教育"},
-		{Name: "住房"},
-		{Name: "工资"},
-		{Name: "奖金"},
-		{Name: "其他"},
-	}
+	foodCat := &models.Category{Name: "饮食"}
+	DB.Create(foodCat)
+	trafficCat := &models.Category{Name: "交通"}
+	DB.Create(trafficCat)
+	DB.Create(&models.Category{Name: "购物"})
+	DB.Create(&models.Category{Name: "娱乐"})
+	DB.Create(&models.Category{Name: "医疗"})
+	DB.Create(&models.Category{Name: "教育"})
+	DB.Create(&models.Category{Name: "住房"})
+	salaryCat := &models.Category{Name: "工资"}
+	DB.Create(salaryCat)
+	DB.Create(&models.Category{Name: "奖金"})
+	DB.Create(&models.Category{Name: "其他"})
 
-	for _, cat := range defaultCategories {
-		DB.Create(&cat)
-	}
-
-	DB.Create(&models.Category{Name: "水果", ParentID: &defaultCategories[0].ID})
-	DB.Create(&models.Category{Name: "外卖", ParentID: &defaultCategories[0].ID})
-	DB.Create(&models.Category{Name: "公交", ParentID: &defaultCategories[1].ID})
-	DB.Create(&models.Category{Name: "地铁", ParentID: &defaultCategories[1].ID})
+	DB.Create(&models.Category{Name: "水果", ParentID: &foodCat.ID})
+	DB.Create(&models.Category{Name: "外卖", ParentID: &foodCat.ID})
+	DB.Create(&models.Category{Name: "公交", ParentID: &trafficCat.ID})
+	DB.Create(&models.Category{Name: "地铁", ParentID: &trafficCat.ID})
 
 	now := time.Now()
 	DB.Create(&models.Bill{
 		Type:       models.BillTypeExpense,
 		Amount:     25.5,
-		CategoryID: defaultCategories[0].ID,
+		CategoryID: foodCat.ID,
 		Time:       now,
 		Remark:     "午餐",
 	})
 	DB.Create(&models.Bill{
 		Type:       models.BillTypeIncome,
 		Amount:     10000,
-		CategoryID: defaultCategories[7].ID,
+		CategoryID: salaryCat.ID,
 		Time:       now.AddDate(0, 0, -5),
 		Remark:     "工资收入",
 	})
